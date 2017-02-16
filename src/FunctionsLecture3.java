daeaa1bb577f416948c5fba074ad8283ccfb0732
jessehartloff <hartloff@buffalo.edu>
2017-02-15 23:28:35 -0500

    initial commit

---
+++ src/FunctionsLecture3.java
@@ -0,0 +1,36 @@
+public class FunctionsLecture3 {
+
+
+
+    // 2 points
+    static boolean Q1(double m, double n) {
+
+        // TODO: return true if m is more than double the value of n, false otherwise
+
+
+        return true;
+    }
+
+
+    // 3 points
+    static String Q2(int start, int stop) {
+
+        // TODO: Return a String containing all the numbers from start to stop (including start and stop).
+        //       Whitespace will be ignored during testing.
+        //
+        // Example: Q2(6, 10) should return "6 7 8 9 10" or "678910"
+
+
+        return "";
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
